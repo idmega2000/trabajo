da5b606e8663d7dfe8377cf0e6b3e4c7648d9014
Idris Kelani <idmega2000@gmail.com>
2019-11-03 13:45:54 +0100

    set up typescript

--- index.js
+++ index.js
@@ -1,47 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-require('dotenv').config()
-const app = express();
-
-const dev = process.env.NODE_ENV
-
-let corsOptions = {
-    origin: process.env.FRONT_END_URL,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-app.use(cors(corsOptions));
-
-app.use(morgan('dev'));
-
-
-app.use(express.json());
-
-const PORT = process.env.PORT || 8000;
-
-// / catch 404 and forward to error handler
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
-  /* eslint-disable-next-line */
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-      errors: {
-        message: err.message,
-      },
-    });
-  });
-
-app.listen(PORT, () => {
-    console.log(
-        `> Server listening at http://localhost:${PORT} as ${
-          dev ? 'development' : process.env.NODE_ENV
-        }`
-    );
-})
-
-
-export default app;
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+require('dotenv').config();
+var app = express_1["default"]();
+var dev = process.env.NODE_ENV;
+// let corsOptions = {
+//     origin: process.env.FRONT_END_URL,
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+// app.use(morgan('dev'));
+// app.use(express.json());
+var PORT = process.env.PORT || 8000;
+/* eslint-disable-next-line */
+// app.use((err, req, res) => {
+//   res.status(err.status || 500).json({
+//     errors: {
+//       message: err.message,
+//     },
+//   });
+// });
+app.listen(PORT, function () {
+    console.log("> Server listening at http://localhost:" + PORT + " as " + (dev ? 'development' : process.env.NODE_ENV));
+});
+exports["default"] = app;
